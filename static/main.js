ad5144a8513deb16beb5a75d8ac1f2db43f62f8d
stanley <stanleyctg@yahoo.com>
2024-03-08 23:26:58 +0000

    Added some images.

--- static/main.js
+++ static/main.js
@@ -1,34 +1,35 @@
 $(document).ready(function(){
-    $('#search_stock').on('submit', function(e){
+    $('#search_stock').on('submit', function(e){ //The button where submit is pressed
         e.preventDefault(); // Prevent the default form submission
         $.ajax({
             url: '/search',
             type: 'post',
             dataType: 'json',
-            data: $(this).serialize(), // Serialize the form data
-            success: function(response) {
+            data: $(this).serialize(), // Serialize the form data .//Think of this as okay I want to connect this script to the backend
+                                        //the url i want to connect to is /search, type is post and datatype is json, we then serialise this form
+                                        //The serialisation is act of connection to the backend
+            success: function(response) { //Upon successful response from the backend it is now connected as backened sends data upon retieving from form
                 var total = response.total
                 var resultHtml = `
                     <div class="search-result">
                         <p>Symbol: ${response.searched_symbol}</p>
                         <input type="number" id="quantity" name="quantity" min="1" value="1">
-                        <button id="buy-btn">Buy</button>
+                        <button id="buy-btn" onclick="buyStock()">Buy</button>
                         <p>Total: $<span id="total">${total}</span></p>
                     </div>    
                 `;
         
-                $('#search-results').html(resultHtml);
+                $('#search-results').html(resultHtml); //The div with class search-result will render resultHtml
 
-                $('#quantity').on('input', function(){
-                    var quantity = $(this).val();
-                    $('#total').text((quantity * total).toFixed(2));
+                $('#quantity').on('input', function(){ //Increment something when to quantity increase, id will have #, then datatype
+                    var quantity = $(this).val(); //The quantity will take value from the id of data type input
+                    $('#total').text((quantity * total).toFixed(2)); //the text of id=total will equal to quantity multiply total 
                 });
             }
         });
     });
 });
 
-function buyStock(symbol) {
-    const quantity = $(`#quantity-${symbol}`).val();
-    alert(`Buying ${quantity} of ${symbol}`);
+function buyStock() {
+    alert(`executing`);
 }
